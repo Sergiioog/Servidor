94833b67ad0b818554a854c9deef43f6038749f9
sergiioog <sergiogargor9@gmail.com>
2024-02-02 11:38:50 +0100

    Cambios inicio

--- servidor.mjs
+++ servidor.mjs
@@ -1,22 +1,17 @@
 import http from 'node:http';
 import fs from 'node:fs'
-// import enrutador from './enrutador.mjs';
 
 const HOST = '127.0.0.1';
 const PORT = 3000;
 
-const SERVER = http.createServer( (req,res) => {
-    
-    /*------REGISTROS------*/
-    let archivoReg = fs.createWriteStream('registros.txt', {flags: 'a'});
-    archivoReg.write(
-        "Hola caracola"
-    )
-    archivoReg.end();
-    /*------REGISTROS------*/
-
-})
-
-SERVER.listen(PORT, HOST, () => {
-    console.log(`Servidor corriendo en http://${HOST}:${PORT}/`)
+const server = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type' : 'text/plain'});
+    res.end("Hola que tal estas")
 });
+
+server.listen(3000, () => {
+   console.log(`Escuando el servidor en el puerto ${PORT} y en la dirección
+    http://${HOST}:${PORT}/`)
+});
+
+
