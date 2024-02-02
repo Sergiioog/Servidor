7e9320b0c53915a89057ed04f5c0f854e4bcff39
sergiioog <sergiogargor9@gmail.com>
2024-02-02 13:53:51 +0100

    Links a paginas

--- servidor.mjs
+++ servidor.mjs
@@ -9,16 +9,52 @@ const PORT = 3000;
 const stream = fs.createWriteStream('./logs/registros.txt', { flags: 'a' });
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    
 
-    
-    
-    escribirFichero(stream,PORT,HOST); //Funcion para escribir el fichero importada
+    if (req.url === "/") {
+
+        fs.readFile('./pages/index.html', 'utf-8', (error, data) => {
+            if (error) {
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Error interno del servidor " + error);
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(data);
+            }
+        });
+
+    } else if (req.url === "/pages/pagina1.html") {
+
+        fs.readFile('./pages/pagina1.html', 'utf-8', (error, data) => {
+            if (error) {
+                res.writeHead(500, {"Content-Type":"text/plain"});
+                res.end("Error interno del servidor: " + error);
+            } else {
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(data);
+            }
+        });
+
+    } else {
+        
+        fs.readFile('./pages/404.html', 'utf-8', (error, data) => {
+            if (error) {
+                console.log("Lo sentimos, la página de error no se pudo mostrar")
+                res.writeHead(500, {"Content-Type": "text/plain"});
+                res.end("Error interno del servidor");
+            } else {
+                res.writeHead(200, {"Content-Type": "text/html"});
+                res.end(data);
+            }
+         });
+    }
 
 
-    res.end("Hola que tal estas");
+
+
+
+    escribirFichero(stream, PORT, HOST); // Función para escribir el fichero importada
 });
+
 
 server.listen(PORT, HOST, () => {
     console.log(`Escuchando el servidor en el puerto ${PORT} y en la dirección http://${HOST}:${PORT}/`);
